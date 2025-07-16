bf1c3dccda2800e96c3b2dad7b3c0f0682a683be
Pericles S. da C. Jr <pericles.scj@gmail.com>
2025-07-16 19:32:14 -0300

    Add cluster module

--- modules.tf
+++ modules.tf
@@ -4,3 +4,12 @@ module "eks_network" {
   project_name = var.project_name
   tags         = var.tags
 }
+
+module "eks_cluster" {
+  source           = "./modules/cluster"
+  project_name     = var.project_name
+  cluster_version  = var.cluster_version
+  tags             = var.tags
+  public_subnet_1a = module.eks_network.pub_subnet_1a
+  public_subnet_1b = module.eks_network.pub_subnet_1b
+}
