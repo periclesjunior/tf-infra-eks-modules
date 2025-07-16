896f923f5a30d95fa89a7e9615958a4be635db81
Pericles S. da C. Jr <pericles.scj@gmail.com>
2025-07-16 19:42:16 -0300

    Add managed node group module

--- modules.tf
+++ modules.tf
@@ -12,3 +12,12 @@ module "eks_cluster" {
   public_subnet_1a = module.eks_network.pub_subnet_1a
   public_subnet_1b = module.eks_network.pub_subnet_1b
 }
+
+module "eks_managed_node_group" {
+  source            = "./modules/managed-node-group"
+  project_name      = var.project_name
+  cluster_name      = module.eks_cluster.cluster_name
+  private_subnet_1a = module.eks_network.priv_subnet_1a
+  private_subnet_1b = module.eks_network.priv_subnet_1b
+  tags              = var.tags
+}
