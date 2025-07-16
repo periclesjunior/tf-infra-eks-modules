9ad2386d10066a6f580544cbe26baeb576ac465c
Pericles S. da C. Jr <pericles.scj@gmail.com>
2025-07-16 18:44:09 -0300

    Update deprecated attribute

--- modules/network/vpc.private-subnets.tf
+++ modules/network/vpc.private-subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_private_subnet_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
-  availability_zone = "${data.aws_region.current.name}a"
+  availability_zone = "${data.aws_region.current.region}a"
 
   tags = merge(
     var.tags,
@@ -15,7 +15,7 @@ resource "aws_subnet" "eks_private_subnet_1a" {
 resource "aws_subnet" "eks_private_subnet_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
-  availability_zone = "${data.aws_region.current.name}b"
+  availability_zone = "${data.aws_region.current.region}b"
 
   tags = merge(
     var.tags,
--- modules/network/vpc.public-subnets.tf
+++ modules/network/vpc.public-subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "eks_public_subnet_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "${data.aws_region.current.name}a"
+  availability_zone       = "${data.aws_region.current.region}a"
   map_public_ip_on_launch = true
 
   tags = merge(
@@ -16,7 +16,7 @@ resource "aws_subnet" "eks_public_subnet_1a" {
 resource "aws_subnet" "eks_public_subnet_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "${data.aws_region.current.name}b"
+  availability_zone       = "${data.aws_region.current.region}b"
   map_public_ip_on_launch = true
 
   tags = merge(
