d7ae62c74c9e5a4109678c827fe1cfd1b14b246c
Pericles Junior <pericles_scj@gmail.com>
2026-03-27 18:50:21 -0300

    Add aws lb module

--- modules.tf
+++ modules.tf
@@ -21,3 +21,12 @@ module "eks_managed_node_group" {
   private_subnet_1b = module.eks_network.priv_subnet_1b
   tags              = var.tags
 }
+
+module "eks_aws_load_balancer_controller" {
+  source       = "./modules/aws-load-balancer-controller"
+  project_name = var.project_name
+  tags         = var.tags
+  oidc         = module.eks_cluster.oidc
+  cluster_name = module.eks_cluster.cluster_name
+  vpc_id       = module.eks_network.vpc_id
+}
