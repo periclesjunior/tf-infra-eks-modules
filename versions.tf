0a84b3c95e49758af42175254484f5c4d1a361f0
Pericles Junior <pericles_scj@gmail.com>
2026-04-08 18:20:08 -0300

    Remove required_version and change hashicorp/aws provider

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.3.2"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.4.0"
+      version = "6.39.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
