2bf02b158fdcddb016ce1ce8c9d9027915073a5a
Pericles Junior <pericles_scj@gmail.com>
2026-04-10 16:43:56 -0300

    Create launch template settings

---
+++ modules/managed-node-group/eks.cluster.launch-template.tf
@@ -0,0 +1,14 @@
+resource "aws_launch_template" "eks_nodes" {
+  name_prefix = "eks-storage-nodes-"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 20
+      volume_type = "gp3"
+      iops        = 3000
+      throughput  = 150
+      encrypted   = true
+      delete_on_termination = true
+    }
+  }
