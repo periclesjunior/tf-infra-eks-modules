414df920d8e325c4b2dcb76b02f4181b90c7d092
Pericles Junior <pericles_scj@gmail.com>
2026-04-10 17:29:57 -0300

    Remove bootstrap extra args

--- modules/managed-node-group/eks.cluster.launch-template.tf
+++ modules/managed-node-group/eks.cluster.launch-template.tf
@@ -1,8 +1,6 @@
 resource "aws_launch_template" "eks_nodes" {
   name_prefix = "eks-storage-nodes-"
 
-  bootstrap_extra_args   = "--use-max-pods=false --kubelet-extra-args=--max-pods=110"
-
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
