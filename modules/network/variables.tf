9b4e34b546e3b3b3261739ea5a12c0c6c3511658
Pericles Junior <pericles_scj@gmail.com>
2026-04-18 12:50:32 -0300

    Refactoring variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,14 +1,14 @@
 variable "cidr_block" {
-  type        = string
   description = "Networking CIDR block to be used for the VPC"
+  type        = string
 }
 
 variable "project_name" {
-  type        = string
   description = "Project name to be used to name the resources (Name tag)"
+  type        = string
 }
 
 variable "tags" {
-  type        = map(any)
   description = "Tags to be added to AWS resources"
+  type        = map(any)
 }
