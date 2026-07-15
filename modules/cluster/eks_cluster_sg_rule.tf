0a61da9addad1c5573c036555a4aa95e786006a6
Pericles Junior <pericles.scj@gmail.com>
2026-07-15 00:56:16 -0300

    Add sg description

--- modules/cluster/eks_cluster_sg_rule.tf
+++ modules/cluster/eks_cluster_sg_rule.tf
@@ -1,8 +1,9 @@
 resource "aws_security_group_rule" "eks_cluster_sg_rule" {
-  type      = "ingress"
-  from_port = 443
-  to_port   = 443
-  protocol  = "tcp"
+  description = "Allow HTTPS access to the EKS control plane endpoint"
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
   ]
